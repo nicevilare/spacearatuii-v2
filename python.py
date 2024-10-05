3ac389a33ee8893d0caa7fba3c7fb9f0bce60c46
eduuuxx97 <edrodriguess97@gmail.com>
2024-10-05 14:24:05 -0300

    Update python.py

--- python.py
+++ python.py
@@ -1,6 +1,26 @@
 from flask import Flask, jsonify, render_template, request
+import cv2 as cv
+import numpy as np
+import rasterio
+import time
+import os
+import shutil
 import ee 
 import geemap 
+import gdown
+from constants import LOC, IMAGE_COLLECTION, BANDS, START_DATE, END_DATE, CITY
+
+import quickstart 
+
+from pydrive.auth import GoogleAuth
+from pydrive.drive import GoogleDrive
+
+# BANDS = ', '.join(f"'{band}'" for band in BANDS)
+
+path = 'img'
+file_id = ''
+
+print(BANDS)
 
 app = Flask(__name__)
 
@@ -16,17 +36,60 @@ ee.Initialize(project='aratu-436806')
 def index():
 
    Map = geemap.Map()
-   landsat_image = ee.ImageCollection('LANDSAT/LC08/C02/T1_L2') \
-        .filterBounds(ee.Geometry.Point([-8.0476, -34.8770])) \
-        .filterDate('2021-01-01', '2021-12-31') \
-        .median()
-   Map.addLayer(landsat_image, {'bands': ['SR_B4', 'SR_B3', 'SR_B2'], 'min': 0, 'max': 3000}, 'Landsat RGB')
-   Map.setCenter(-47.9292, -15.7801, 4)
+
+   all_cities = ee.ImageCollection([])
+   one_city = ee.ImageCollection([])
+   if not CITY:
+       for _, coord in LOC.items():
+        landsat_image = ee.ImageCollection(IMAGE_COLLECTION) \
+                .filterBounds(ee.Geometry.Point(coord)) \
+                .filterDate(START_DATE, END_DATE) \
+                .median()
+        all_cities = all_cities.merge(landsat_image)
+   else:
+      landsat_image = ee.ImageCollection(IMAGE_COLLECTION) \
+                .filterBounds(ee.Geometry.Point(list(CITY.values())[0])) \
+                .filterDate(START_DATE, END_DATE) \
+                .median()
+      one_city = one_city.merge(landsat_image)
+
+   #export_to_drive(landsat_image, 'Natal')
+   
+   name= 'Landsat_Image_Export' + '_Landsat_Img_.tif'
+   #print(name)
+   #download_img(quickstart.main(name))
+
+
+   Map.addLayer(all_cities if not CITY else one_city, {'bands': BANDS, 'min': 1000, 'max': 30000}, 'Landsat RGB')
+   Map.setCenter(-47.9292, -15.7801, 4)  # Longitude, Latitude, Zoom level
 
    map_html = Map.to_html()  
 
    return render_template('index.html', map_html=map_html)
 
+
+def create_dir(name, img):
+    """
+    create directory
+    """
+    if not os.path.exists(path):
+      os.path.makedirs(path)
+    file_path = path + '/' + name + '.txt'
+#    with open(file_path, 'w') as file:
+#       file.write(str(img))
+    return file_path
+
+def convert_to_png(name):
+   """
+   try to convert .tif to png :) 
+   """
+   with rasterio.open(name) as src:
+      img_array = src.read()
+   first_band_array = img_array[0, :, :]
+   first_band_array = cv.normalize(first_band_array, None, 0, 255, cv.NORM_MINMAX)
+   first_band_array = first_band_array.astype(np.uint8)
+   cv.imwrite('img/landsat_img_converted.png', first_band_array)
+ 
 
 if __name__ == '__main__':
     app.run(debug=True)
